86be7f4e1bc631d38f1525848f382eb4ddad6d31
ClearJulyComes <55094042+ClearJulyComes@users.noreply.github.com>
2020-06-01 08:16:05 +0300

    Second commit to firstproject
    Ajax method to auth and reg

--- firstproject/src/main/java/MainController.java
+++ firstproject/src/main/java/MainController.java
@@ -1,18 +1,34 @@
-/*
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@Path("/")
+@Path("/auth")
 public class MainController {
+
+    @POST
+    public void authMethod(@Context HttpServletRequest request, @Context HttpServletResponse response) throws ServletException, IOException {
+        /*response.setContentType("text/html");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.println("<h2>Hello from HelloServlet</h2>");
+        } */
+        response.sendRedirect("https://google.com/");
+    }
+
     @GET
-    public void homePage(@Context HttpServletRequest request, @Context HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.jsp")
-                .forward(request, response);
+    public String tat(@Context HttpServletRequest request, @Context HttpServletResponse response) throws ServletException, IOException {
+        /*response.setContentType("text/html");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.println("<h2>Hello from HelloServlet</h2>");
+        } */
+        response.sendRedirect("https://google.com/");
+        return null; //TODO replace this stub to something useful
     }
 }
-*/
--- firstproject/src/main/webapp/js/main.js
+++ firstproject/src/main/webapp/js/main.js
@@ -1,10 +1,23 @@
+
+
+const MyApp = Mn.Application.extend({
+    initialize: function(options) {
+    }
+});
+
+const myApp = new MyApp();
+
+myApp.on('start', function() {
+    mainRegion.show(new Reg());
+});
+
 const MyRegion = Mn.Region.extend({ el: $('#myArea') });
 const mainRegion = new MyRegion();
 
 const Auth = Mn.View.extend({
     template: _.template(`
         <div> 
-            <form class="auth">
+            <form id="auth" method="post">
     <table>
     <tr>
     <td>Login</td>
@@ -20,15 +33,15 @@ const Auth = Mn.View.extend({
     </tr>
     <tr>
     <td>
+    <button id="toRegButton">Registration</button>
     </td>
     <td></td>
     <td>
-    <button id="authButton" type="submit">Ok</button>
+    <button id="authButton">Ok</button>
     </td>
     </tr>
     </table>
     </form>
-    <button id="toRegButton">Registration</button>
         </div>`
     ),
     events: {
@@ -44,6 +57,27 @@ const Auth = Mn.View.extend({
     toRegistration(){
         console.log("Hey");
         mainRegion.show(new Reg());
+    },
+    authorization(){
+        $('#auth').submit(function (e) {
+            e.preventDefault();
+
+            $.ajax({
+                url:   "/auth"  , //url страницы (action_ajax_form.php)
+                type:     "POST", //метод отправки
+                dataType: "json", //формат данных
+                data: $('#auth').serialize(),  // Сеарилизуем объект
+                success: function(response) { //Данные отправлены успешно
+                    console.log("Success");
+                    //    result = $.parseJSON(response);
+                    //    $('#result_form').html('Имя: '+result.name+'<br>Телефон: '+result.phonenumber);
+                },
+                error: function(response) { // Данные не отправлены
+                    console.log("error");
+                    //$('#result_form').html('Ошибка. Данные не отправлены.');
+                }
+            })
+        });
     }
 
 });
@@ -51,7 +85,7 @@ const Auth = Mn.View.extend({
 const Reg = Mn.View.extend({
     template: _.template(`
         <div>
-            <form class="reg">
+            <form id="reg" method="post">
                 <table>
                     <tr>
                         <td>Login</td>
@@ -67,15 +101,15 @@ const Reg = Mn.View.extend({
                     </tr>
                     <tr>
                         <td>
+                        <button id="toAuthButton">Authorization</button>
                         </td>
                         <td></td>
                         <td>
-                            <button id="regButton" type="submit">Ok</button>
+                            <button id="regButton">Ok</button>
                         </td>
                     </tr>
                 </table>
             </form>
-            <button id="toAuthButton">Authorization</button>
         </div>`
     )  ,
     events: {
@@ -95,4 +129,4 @@ const Reg = Mn.View.extend({
 
 });
 
-mainRegion.show(new Auth());
+myApp.start();
